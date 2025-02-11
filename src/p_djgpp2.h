bd67677389800b2f3e44711ae055e895721208a2
John Reiser <jreiser@BitWagon.com>
2025-02-11 10:29:53 -0800

    Honor dos_header_t.e_cparhdr for small header
    ... and cleanup PackDjgpp2::readFileHeader()
    https://github.com/upx/upx/issues/881
    
    	modified:   p_djgpp2.cpp
    	modified:   p_djgpp2.h

--- src/p_djgpp2.h
+++ src/p_djgpp2.h
@@ -62,6 +62,16 @@ protected:
 
     unsigned coff_offset;
 
+    // based on https://dox.ipxe.org/PeImage_8h_source.html
+    struct alignas(1) dos_header_t {
+        LE16 e_magic;
+        LE16 e_cblp;    // Bytes on last page of file
+        LE16 e_cp;      // Pages in file
+        LE16 e_crlc;    // Relocations
+        LE16 e_cparhdr; // Size of header in paragraphs
+        LE16 e_OMITTED[32 - 5 * 2];
+    }; // 64 bytes
+
     struct alignas(1) external_scnhdr_t {
         byte _[12]; // name, paddr
         LE32 vaddr;
